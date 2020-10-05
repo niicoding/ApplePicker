3dd0088a3b32e760d63b275ec060a427030d8a18
Zachary Perales <zackaryperales@gmail.com>
2020-10-05 09:31:34 -0400

    Refactored. Next commit is last, need to just fix the rotation bug where if it rotates with the same # of baskets on screen a falling apple off screen of the same color will subtract a basket.

--- Assets/Scripts/Apple.cs
+++ Assets/Scripts/Apple.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class Apple : MonoBehaviour {
     private static float bottomY = -20f; // Below visible screen.
-    private static List<float> dropSpeedList = new List<float> { -1f, -1f, -1f };
+    private static List<float> dropSpeedList = new List<float> { -1f, -100f, -200f };
     private GameObject apple;
     private Color appleColor;
     private ApplePicker apScript;
@@ -17,8 +17,8 @@ public class Apple : MonoBehaviour {
         SetDifficulty(GetComponent<Rigidbody>(), GetComponent<Renderer>(), int.Parse(Basket.ScoreGT.text));
     }
     private void Update() {
-        if (apScript.basketList.Count != 0) {                     
-            Color basketColor = apScript.basketList[apScript.basketList.Count - 1].GetComponent<Renderer>().material.color;
+        if (apScript.BasketList.Count != 0) {                     
+            Color basketColor = apScript.BasketList[apScript.BasketList.Count - 1].GetComponent<Renderer>().material.color;
             if (transform.position.y < bottomY && !apScript.ExtraHard) {  // If offscreen, delete apple.
                 Destroy(apple);
                 apScript.AppleDestroyed(); // Get a reference to the Apple Picker component of the Main Camera. Call the public AppleDestroyed() method of apScript
@@ -34,7 +34,7 @@ public class Apple : MonoBehaviour {
                 rigidBody.AddForce(rigidBody.transform.up * dropSpeedList[1]); // Change apple drop speed to -100f.
                 renderer.material.color = Color.yellow; // Change apple color to yellow.
                 break;
-            case var expression when (Random.Range(0, 2)) * scoreNumeric > 2500:  // 25% chance of executing when score >= 2000. Hard difficulty
+            case var expression when (Random.Range(0, 2)) * scoreNumeric > 2500:  // 25% chance of executing when score >= 2000. Hard difficulty. 25% red chance.
                 rigidBody.AddForce(rigidBody.transform.up * dropSpeedList[2]); // Change apple drop speed to -200f.
                 renderer.material.color = Color.green; // Change apple color to green.
                 break;
--- Assets/Scripts/ApplePicker.cs
+++ Assets/Scripts/ApplePicker.cs
@@ -5,35 +5,34 @@ using System.Xml.Schema;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class ApplePicker : MonoBehaviour {
-    private static bool extraHard = false;
-    public bool ExtraHard {
-        get { return extraHard; }
-        set { extraHard = value; }
-    }
-
-    [Header("Set in Inspector")]
-    public GameObject basketPrefab;
-    public int numBaskets = 3;
-    public float basketBottomY = -14f, basketSpacingY = 2f;
-    public List<GameObject> basketList;
     private static List<Color> colourList = new List<Color> { Color.red, Color.yellow, Color.green };
-    void Start() {        
-        basketList = new List<GameObject>();
+    private static bool extraHard = false;
+    public bool ExtraHard { get => extraHard; set => extraHard = value; }
+    public int NumBaskets { get => numBaskets; set => numBaskets = value; }
+    public List<GameObject> BasketList { get => basketList; set => basketList = value; }
+    // [Header("Set in Inspector")] No. 
+    public GameObject basketPrefab;
+    private int numBaskets = 3;
+    private static float basketBottomY = -14f, basketSpacingY = 2f;
+    private static List<GameObject> basketList;
+
+    private void Start() {        
+        BasketList = new List<GameObject>();
         for (int i = 0; i < numBaskets; i++) {
             GameObject tBasketGO = Instantiate<GameObject>(basketPrefab);
             Vector3 pos = Vector3.zero;
             pos.y = basketBottomY + (basketSpacingY*i);
             tBasketGO.transform.position = pos;
-            basketList.Add(tBasketGO);
+            BasketList.Add(tBasketGO);
         }
     }
-    void Update() {
-        if (extraHard && basketList.Count != 0)
-                if (!colourList.Contains(basketList[0].GetComponent<Renderer>().material.color))
+    private void Update() {
+        if (extraHard && BasketList.Count != 0)
+                if (!colourList.Contains(BasketList[0].GetComponent<Renderer>().material.color))
                     StartExtraHardMode();
     }
-    void StartExtraHardMode() {
-        InitializeColours(basketList);
+    private void StartExtraHardMode() {
+        InitializeColours(BasketList);
         Invoke(nameof(RotateColours), 5f);
     }
     public void AppleDestroyed() {
@@ -43,39 +42,40 @@ public class ApplePicker : MonoBehaviour {
         BasketDestroyed();
     }
 
-    public void DestroyApples() {
+    private void DestroyApples() {
         GameObject[] tAppleArray = GameObject.FindGameObjectsWithTag("Apple");
         foreach (GameObject tGO in tAppleArray) // Destroy all of the falling apples.
             Destroy(tGO);
     }
 
     public void BasketDestroyed() {
-        int basketIndex = basketList.Count - 1; // Get the index of the last Basket in basketList, destroy.
-        GameObject tBasketGO = basketList[basketIndex]; // Get a reference to that Basket GameObject
-        basketList.RemoveAt(basketIndex); // Remove the Basket from the list.
+        int basketIndex = BasketList.Count - 1; // Get the index of the last Basket in basketList, destroy.
+        GameObject tBasketGO = BasketList[basketIndex]; // Get a reference to that Basket GameObject
+        BasketList.RemoveAt(basketIndex); // Remove the Basket from the list.
 
         Destroy(tBasketGO); // Destroy GameObject.
-        if (basketList.Count == 0)
+        if (BasketList.Count == 0)
         { // If there are no Baskets left, restart the game.
             Basket.scoreGT.text = "0";
             extraHard = false;
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
-    void InitializeColours(List<GameObject> basketList) {
+    private void InitializeColours(List<GameObject> basketList) {
         DestroyApples();
             foreach (Color colour in colourList)
                 if (colourList.IndexOf(colour) < basketList.Count)
                     basketList[colourList.IndexOf(colour)].GetComponent<Renderer>().material.color = colour;
     }
-    void RotateColours() {
+    private void RotateColours() {
         DestroyApples();
         Color basketColour;
-        foreach (GameObject basket in basketList) {
+        foreach (GameObject basket in BasketList) {
             basketColour = basket.GetComponent<Renderer>().material.color;
             basketColour = colourList[(colourList.IndexOf(basketColour) + 1 == 3) ? 0 : colourList.IndexOf(basketColour) + 1];
             basket.GetComponent<Renderer>().material.color = basketColour;
         }
-        Invoke(nameof(RotateColours), 5f);
+        DestroyApples();
+        Invoke(nameof(RotateColours), 10f);
     }
 }
